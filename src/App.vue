b7dbe8708ca4d5c7bb4c329dac8c4fc656b023e0
Fallet Steve <steve.fallet@divtec.ch>
2023-11-14 16:46:24 +0100

    Fin étape 2 sans tous les composants et sans intégration CSS dans composants

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <script setup>
 // Cheat Sheet: https://steve-fallet.notion.site/Vue-3-script-setup-Cheat-Sheet-b12192ceae244ecda65f771579ca02bc
 import {ref, onMounted} from 'vue'
-
+import PageFooter from "@/components/PageFooter.vue";
+import PageTopBar from "@/components/PageTopBar.vue";
+import TroupeCarte from "@/components/TroupeCarte.vue";
+/* Données */
+// Total Or
+const totalOr = ref(17478)
 // Tableau des troupes
 const troupes = ref([])
+// Tableau des troupes formées
+const troupesFormees = ref([])
 
-// Cycle de vie du composant
+/* Méthodes */
+function formerTroupe(troupe) {
+  if (totalOr.value < troupe.cout) {
+    alert("Vous n'avez pas assez d'or mon seigneur !")
+    return
+  }
+  totalOr.value -= troupe.cout
+  troupesFormees.value.push(troupe)
+}
+
+/* Cycle de vie du composant */
 // Quand le composant est monté, on va chercher les données
 onMounted(() => {
   fetch('https://cocapi.divtec.me/troupes')
@@ -17,16 +34,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <aside class="solde-or">
-    <div>
-      <img src="/img/piece-or-note.jpg" alt="Solde Or">
-      20 000 pièces d'or
-    </div>
-    <div>
-      <img src="/img/troupes-icon.png" alt="Troupes">
-      0 troupes formées
-    </div>
-  </aside>
+  <page-top-bar :or="totalOr" :troupes="troupesFormees"/>
   <header>
     <h1>
       <img src="/img/clash-of-clans-logo.webp" alt="Logo Clash of Clans">
@@ -42,46 +50,13 @@ onMounted(() => {
         v-for="laTroupe in troupes"
         :key="laTroupe.id"
       >
-        <article>
-          <header :style="`background: linear-gradient(60deg,#3B3B3B 0%, ${ laTroupe.couleur } 100%);`">
-            <img
-                :src="laTroupe.image"
-                :alt="laTroupe.nom"
-            >
-          </header>
-          <div class="level" :style="`color: ${ laTroupe.couleur };`">
-            Niveau {{ laTroupe.niveau }}
-          </div>
-          <h2 class="name">{{ laTroupe.nom }}</h2>
-          <button :style="`background-color: ${laTroupe.couleur}`">
-            Former
-            <img src="/img/piece-or.png" alt="Former">
-          </button>
-          <p class="description">{{ laTroupe.description }}</p>
-          <footer>
-            <div class="training"
-                 :style="`background-color: ${laTroupe.couleur}`">
-              <div>{{ laTroupe.formation }}<sup>sec</sup></div>
-              <div>Formation</div>
-            </div>
-            <div class="speed"
-                 :style="`background-color: ${laTroupe.couleur}`">
-              <div>{{ laTroupe.vitesse }}</div>
-              <div>Vitesse</div>
-            </div>
-            <div class="cost"
-                 :style="`background-color: ${laTroupe.couleur}`">
-              <div>{{ laTroupe.cout }}</div>
-              <div>Coût</div>
-            </div>
-          </footer>
-        </article>
+        <troupe-carte :troupe="laTroupe"
+                      :or="totalOr"
+                      @former="formerTroupe"/>
       </li>
     </ul>
   </main>
-  <footer>
-    &copy; 2023 - Supercell.com
-  </footer>
+  <page-footer />
 </template>
 
 <style scoped lang="sass">
---
+++ src/components/PageFooter.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer>
+    &copy; 2023 - Supercell.com
+    - <a href="mailto:steve.fallet@divtec.ch">Contact</a>
+  </footer>
+</template>
+
+<style scoped lang="sass">
+footer
+  position: fixed
+  z-index: 999
+  bottom: 0
+  height: 60px
+  width: 100%
+  background-color: black
+  text-align: center
+  padding: 1rem
+  margin: 0
+
+  a
+    color: white
+</style>
