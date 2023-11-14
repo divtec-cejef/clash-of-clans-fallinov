c7de3b0798f778e3b005254fa9e26187ce13c201
Steve Fallet <steve.fallet@gmail.com>
2023-11-14 13:27:14 +0100

    ETAPE3

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,59 @@
 <script setup>
-import {RouterLink, RouterView} from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'§
+// Cheat Sheet: https://steve-fallet.notion.site/Vue-3-script-setup-Cheat-Sheet-b12192ceae244ecda65f771579ca02bc
+import {ref} from 'vue'
+
+// Tableau des troupes
+const troupes = ref([
+  {
+    "id": 2,
+    "nom": "Archer",
+    "niveau": 3,
+    "formation": 25,
+    "vitesse": 24,
+    "cout": 200,
+    "description": "Les archers sont des tireurs d'élite qui attaquent à distance. Ils sont rapides et bon marché à former, mais ils sont faibles en mêlée et doivent être protégés.",
+    "couleur": "#EE5487",
+    "image": "https://cocapi.divtec.me/img/archer.png"
+  },
+  {
+    "id": 1,
+    "nom": "Barbare",
+    "niveau": 4,
+    "formation": 20,
+    "vitesse": 16,
+    "cout": 150,
+    "description": "Ce guerrier intrépide compte sur ses muscles saillants et sa fière moustache pour semer le chaos dans les villages ennemis. Faites charger une horde de barbares et profitez du spectacle !",
+    "couleur": "#ebb428",
+    "image": "https://cocapi.divtec.me/img/barbare.png"
+  },
+  {
+    "id": 3,
+    "nom": "Géant",
+    "niveau": 5,
+    "formation": 12,
+    "vitesse": 12,
+    "cout": 2250,
+    "description": "Les géants sont de grands guerriers qui attaquent les défenses ennemies en premier. Ils sont lents mais résistants.",
+    "couleur": "#F6901A",
+    "image": "https://cocapi.divtec.me/img/geant.png"
+  }
+])
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125"
-         height="125"/>
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!"/>
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView/>
+  <h1>Clash Of Clans</h1>
+  <ul>
+    <li
+        v-for="troupe in troupes"
+        :key="troupe.id"
+        :style="`background-color: ${troupe.couleur};`"
+    >
+      {{ troupe.nom }}
+      <img :src="troupe.image" :alt="troupe.nom">
+    </li>
+  </ul>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
